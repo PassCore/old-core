d11c20856bc7023376c7166a7d67a360c2c69df9
深度学习 <33132228+Kucashu@users.noreply.github.com>
2019-10-26 13:06:51 +0800

    Add run.sh

--- script/run.bat
+++ script/run.bat
@@ -1,2 +1,7 @@
 @echo off
+
+::Passcore runner
+::Author: Kevin
+::Date: 20191026
+
 dotnet core\core.dll
---
+++ script/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Passcore runner
+#Author: Kevin
+#Date: 20191026
+
+dotnet "core\core.dll"
